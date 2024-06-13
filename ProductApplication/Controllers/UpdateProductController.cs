732f706bb4cf560c74337fa92f67cdeec747625b
DESKTOP-EB46TJV\Tingyu <jenny2006nono@gmail.com>
2024-06-13 18:18:58 +0800

    更新商品資訊

---
+++ ProductApplication/Controllers/UpdateProductController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using ProductApplication.Models;
+using ProductApplication.ViewModel;
+
+namespace ProductApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UpdateProductController : ControllerBase
+    {
+        private readonly ShopDbContext _context;
+
+        public UpdateProductController(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        // PUT: api/updateproduct/productId
+        [HttpPost]
+        public async Task<IActionResult> UpdateProduct(int productId, [FromBody] ProductUpdateModel product)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                //搜尋商品
+                var exProduct = _context.Product.Where(p =>p.ProductID == productId).FirstOrDefault();
+
+                if (exProduct == null)
+                {
+                    return NotFound($"商品編號{productId}不存在");
+                }
+
+                // 更新商品資訊
+                exProduct.ProductName = product.ProductName ?? exProduct.ProductName;
+                exProduct.Description = product.Description ?? exProduct.Description;
+                exProduct.Price = product.Price ?? exProduct.Price;
+                exProduct.Quantity = product.Quantity ?? exProduct.Quantity;
+                exProduct.CategoryID = product.CategoryID ?? exProduct.CategoryID;
+                exProduct.UpdateTime = DateTime.Now;
+                exProduct.IsActive = product.IsActive ?? exProduct.IsActive;
+
+                _context.Product.Update(exProduct);
+                await _context.SaveChangesAsync();
+
+                return Ok(exProduct);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "更新商品失敗");
+            }
+        }
+    }
+
+}
---
+++ ProductApplication/ViewModel/ProductUpdateModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ProductApplication.ViewModel
+{
+    public class ProductUpdateModel
+    {
+        [Key]
+        public int ProductID { get; set; }   // 商品ID
+
+        public string? ProductName { get; set; }   // 商品名稱
+
+        public string? Description { get; set; }   // 商品描述
+
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal? Price { get; set; }   // 價格
+
+
+        public int? Quantity { get; set; }   // 庫存數量
+
+        public int? CategoryID { get; set; }// 商品分類ID，預設1
+
+        public DateTime DateTime { get; set; }    // 新增日期，默認為當前時間
+
+        public DateTime UpdateTime { get; set; } = DateTime.Now;   // 更新日期，更新時自動更改
+
+        public bool? IsActive { get; set; }   // 是否上架，預設上架
+    }
+}
